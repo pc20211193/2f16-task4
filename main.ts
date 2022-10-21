0daede480faba4ab05db9a43a876b2b3f7f11667
walter hartwell white <115199250+pc20211193@users.noreply.github.com>
2022-10-21 09:40:44 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,28 +1,24 @@
-input.onButtonPressed(Button.A, function () {
-    while (hi <= 5) {
-        led.plot(hi, 0)
-        basic.pause(500)
-        hi += 1
+let hi = 0
+let bye = 0
+basic.forever(function () {
+    if (input.buttonIsPressed(Button.B)) {
         basic.clearScreen()
-        led.plot(hi, 0)
-    }
-    if (hi >= 5) {
-        hi = 0
-    }
-})
-input.onButtonPressed(Button.B, function () {
-    while (bye <= 5) {
         led.plot(0, bye)
         basic.pause(500)
         bye += 1
-        basic.clearScreen()
-        led.plot(0, bye)
     }
-    if (bye >= 5) {
+    if (bye > 4) {
         bye = 0
     }
 })
-let bye = 0
-let hi = 0
-hi = 0
-bye = 0
+basic.forever(function () {
+    if (input.buttonIsPressed(Button.A)) {
+        basic.clearScreen()
+        led.plot(hi, 0)
+        basic.pause(500)
+        hi += 1
+    }
+    if (hi > 4) {
+        hi = 0
+    }
+})
